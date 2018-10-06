67c41e9c388e6ce7fc4e34cb8394f8c6d95240d0
shannonsuhendra <shannon@suhendra.com>
2018-10-06 23:58:05 +0800

    added accordian

--- screens/UpdateScreen.js
+++ screens/UpdateScreen.js
@@ -1,14 +1,80 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from 'react-native';
+import Accordion from 'react-native-collapsible/Accordion';
+
+const SECTIONS = [
+    {
+      title: 'First',
+      content: 'hiiiiii'
+    },
+    {
+      title: 'Second',
+      content: 'helllooo'
+    }
+  ];
 
 export default class UpdateScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
+  
+    state = {
+        activeSections: []
+      };
+     
+      _renderSectionTitle = section => {
+        return (
+          <View style={styles.content}>
+            
+          </View>
+        );
+      };
+     
+      _renderHeader = section => {
+        return (
+          <View style={styles.header}>
+            <Text style={styles.headerText}>{section.title}</Text>
+          </View>
+        );
+      };
+     
+      _renderContent = section => {
+        return (
+          <View style={styles.content}>
+            <Text>{section.content}</Text>
+          </View>
+        );
+      };
+     
+      _updateSections = activeSections => {
+        this.setState({ activeSections });
+      };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+        <View style={styles.container}>
+            <Text> Updates </Text>
+            <Accordion
+                sections={SECTIONS}
+                activeSections={this.state.activeSections}
+                renderSectionTitle={this._renderSectionTitle}
+                renderHeader={this._renderHeader}
+                renderContent={this._renderContent}
+                onChange={this._updateSections}
+              />
+        </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    }
+});
